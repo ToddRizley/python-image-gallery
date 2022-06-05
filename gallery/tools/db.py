85a247a8e6d7012d610e37a99d3a8a6f977651b7
ToddRizley <todd.rizley@gmail.com>
2022-06-05 16:38:02 +0000

    fix connection issues

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -1,81 +1,52 @@
 import psycopg2
 import json
-from gallery.tools.secrets import get_secret_image_gallery
-#from secrets import get_secret_image_gallery
-class DBConnector:
+from .secrets import get_secret_image_gallery
+
+connection = None
+
+def get_secret():
+    jsonString = get_secret_image_gallery()
+    return json.loads(jsonString)
+
+def connect():
+    global connection
+    secret = get_secret()
+    connection = psycopg2.connect(host=secret['host'], dbname=secret['db_name'], user= secret['username'], password= secret['password'])
+    connection.set_session(autocommit=True)
+
+def execute(query, args=None):
+    global connection
+    cursor = connection.cursor()
+    try:
+        if not args:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, args)
+        return cursor
+    except Exception as e:
+        print(e)
+        return False
+
+def add_user(username, password, full_name):
+    res = execute("INSERT INTO users (username, password, full_name) VALUES (%s, %s, %s)", (username, password, full_name) )
+    return res    
     
-    def __init__(self):
-        self.connection = None
-        secret = self.get_secret()
-        self.db_host = secret['host']
-        self.db_name = secret['db_name']
-        self.db_user = secret['username']
-        self.password = secret['password']
+def get_user(username):
+    res = execute("SELECT * FROM users WHERE username=%s", (username,))
+    return res
+
+def get_users():
+    res = execute("SELECT * FROM users")
+    return res
+
+def edit_full_name(username, new_full_name):
+    res = execute("UPDATE users SET full_name=%s where username=%s", (new_full_name, username))
+    return res
+
+def edit_password(username, new_password):
+    res = execute("UPDATE users SET password=%s where username=%s", (new_password, username))
+    return res
     
-    def get_secret(self):
-        jsonString = get_secret_image_gallery()
-        return json.loads(jsonString)
-
-#    def set_db_vars(self, host_in, name_in, user_in, password_file_in):
-#        self.db_host = host_in
-#        self.db_name = name_in
-#        self.db_user = user_in
-#        self.password_file = password_file_in
-        
-  #  def get_password(self):
-  #      f = open(self.password_file, "r")
-  #      result = f.readline()
-  #      f.close
-  #      return result[:-1]
-
-    def connect(self):
-        self.connection = psycopg2.connect(host=self.db_host, dbname=self.db_name, user= self.db_user, password= self.password)
-        self.connection.set_session(autocommit=True)
-
-    def execute(self, query, args=None):
-        cursor = self.connection.cursor()
-        try:
-            if not args:
-                cursor.execute(query)
-            else:
-                cursor.execute(query, args)
-            return cursor
-        except Exception as e:
-            print(e)
-            return False
-
-    def add_user(self, username, password, full_name):
-        res = self.execute("INSERT INTO users (username, password, full_name) VALUES (%s, %s, %s)", (username, password, full_name) )
-        return res
-    
-    def get_user(self, username):
-        res = self.execute("SELECT * FROM users WHERE username=%s", (username,))
-        return res
-
-    def get_users(self):
-        res = self.execute("SELECT * FROM users")
-        return res
-
-    def edit_full_name(self, username, new_full_name):
-        res = self.execute("UPDATE users SET full_name=%s where username=%s", (new_full_name, username))
-        return res
-
-    def edit_password(self, username, new_password):
-        res = self.execute("UPDATE users SET password=%s where username=%s", (new_password, username))
-        return res
-    
-    def delete_user(self, username):
-        print(username)
-        res = self.execute("DELETE FROM users WHERE username=%s", (username,))
-        return res
-
-# def main():
-#     connect()
-#     res = execute("update users set password=%s where username='fred'", ('banana',))
-#     res = execute('select * from users;')
-#     for row in res:
-#         print(row)
-
-# if __name__ == '__main__':
-#     main()
-
+def delete_user(username):
+    res = execute("DELETE FROM users WHERE username=%s", (username,))
+    return res
--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,51 +1,44 @@
 from flask import Flask, request, render_template, url_for, redirect
-from gallery.tools.db import *
+from gallery.tools.db import connect, get_users, get_user, add_user, edit_full_name, edit_password, delete_user
+#from gallery.tools.user import User
+#from gallery.tools.postgres_user_dao import PostgresUserDAO
 
 app = Flask(__name__)
+connect()
 
 @app.route('/admin')
 def admin():
-    db_instance = DBConnector()
-    db_instance.connect()
-    res  = db_instance.get_users()
-    return render_template('admin.html', users=prepare_user_list(res))
+    users = get_users()
+    return render_template('admin.html', users=prepare_user_list(users))
 
-@app.route('/admin/add_user')
-def add_user():
-    return render_template("add_user.html")
+@app.route('/admin/add_new_user')
+def add_new_user():
+    return render_template("add_new_user.html")
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
-    db_instance = DBConnector()
-    db_instance.connect()
 
     username = request.form['username']
     full_name = request.form['full_name']
     password = request.form['password']
-    db_instance.add_user(username, password, full_name)
+    add_user(username, password, full_name)
 
-    res  = db_instance.get_users()
-    return render_template('admin.html', users=prepare_user_list(res))
+    return  redirect('/admin')
 
 @app.route('/admin/deletion_confirmation/<username>', methods=['GET'])
 def deletion_confirmation(username):
     return render_template("deletion_confirmation.html", username=username)
 
 @app.route('/delete_user/<username>', methods=['POST'])
-def delete_user(username):
-    db_instance = DBConnector()
-    db_instance.connect()
-    db_instance.delete_user(username)
+def delete_user_route(username):
+    delete_user(username)
 
-    res  = db_instance.get_users()
-    return render_template('admin.html', users=prepare_user_list(res))
+    return  redirect('/admin')
 
 @app.route('/admin/edit_user/<username>', methods=['GET'])
 def edit_user(username):
-    db_instance = DBConnector()
-    db_instance.connect()
     
-    row = db_instance.get_user(username).fetchone()
+    row = get_user(username).fetchone()
     user = {'username': row[0], 'password': row[1], 'full_name': row[2]}
     return render_template("edit_user.html", user=user)
 
@@ -54,23 +47,34 @@ def update_user(username):
     full_name = request.form['full_name']
     password = request.form['password']
     
-    db_instance = DBConnector()
-    db_instance.connect()
-
-    row = db_instance.get_user(username).fetchone()
+    row = get_user(username).fetchone()
     user = {'username': row[0], 'password': row[1], 'full_name': row[2]}
 
     if user['password'] != password:
-        db_instance.edit_password(username, password)
+        edit_password(username, password)
     if user['full_name'] != full_name:
-        db_instance.edit_full_name(username, full_name)
+        edit_full_name(username, full_name)
 
-    res  = db_instance.get_users()
-    return render_template('admin.html', users=prepare_user_list(res))
+    return  redirect('/admin')
+
+@app.route('/hello')
+def hello():
+    return '<p>hello world!</p>'
+
+#@app.route('/users')
+#def users():
+#    result = ""
+#    for user in get_user_DAO().get_users():
+#        result+= str(user.username)
+#    return result
+    
 
 #Helper methods
+#def get_user_DAO():
+#    return PostgresUserDAO()
+
 def prepare_user_list(users_object):
     result = []
     for row in users_object:
-        result.append({'username': row[0], 'full_name':row[1]})
+        result.append({'username': row[0], 'full_name':row[2]})
     return result
